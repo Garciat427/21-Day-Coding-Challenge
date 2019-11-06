c9bbbf70121f5f35863189eeff5302432fa54733
sheenagarcia <tg1212@outlook.com>
2019-11-06 00:56:16 -0500

    Added Day 1 and Day 2 Code

---
+++ Day1/Day1.js
@@ -0,0 +1,6 @@
+//Challenge #1 - Door to Door
+
+const doorToDoor = (volunteers, neighbourhoods) => {
+   let numVisits = (neighbourhoods.length / volunteers.length);
+   return numVisits;
+ }
---
+++ Day2/Day2.js
@@ -0,0 +1,13 @@
+//Challenge #2 - Something to Talk About
+
+const interviewAnswer = (topic) => {
+  if (topic === "arts funding") {
+    return ("We'll have to get creative!")
+  } else if (topic === "economy") {
+    return ("Time is money.")
+  } else if (topic === "transportation") {
+    return ("It's going to be a long road, so we better get moving.")
+  } else {
+    return ("QUACK!")
+  }
+}
