1d6afdff85ea85f289918b84495b1f4ba65740e1
sheenagarcia <tg1212@outlook.com>
2019-11-06 23:05:20 -0500

    Adding Day 6

---
+++ Day6/Day6.js
@@ -0,0 +1,13 @@
+//
+const voterTurnout = (voter_signatures, voter_ids) => {
+   if (voter_signatures.length !== voter_ids.length) {
+      return (false);
+   } else {
+      for (let i = 0; i < voter_signatures.length; i++) {
+         if (voter_signatures[i] !== voter_ids[i]) {
+            return ("FRAUD!");
+         }
+      }
+      return ("All clear, we can count the votes!");
+   }
+}
